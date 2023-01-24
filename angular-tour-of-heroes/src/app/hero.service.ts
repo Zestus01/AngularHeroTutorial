a082c89374e720685245925107c86989055b54fb
Zach Estus <zachary.estus@student.awesomeinc.org>
2023-01-24 21:03:55 +0000

    Messages, Injection ngFor, ngIf

---
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heros';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add("HeroService: fetched heroes")
+    return heroes;
+  }
+  constructor(private messageService: MessageService) { }
+}
