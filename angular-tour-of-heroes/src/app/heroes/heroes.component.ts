fa0aff0ceaad606626f67851b6b304c1aeb1ca30
Zach Estus <zachary.estus@student.awesomeinc.org>
2023-01-24 21:56:12 +0000

    Routes, navigation, back

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -11,15 +11,10 @@ import { MessageService } from '../message.service';
 
 export class HeroesComponent implements OnInit{
   heroes: Hero[] = [];
-  selectedHero?: Hero;
+
 
   constructor(private heroService: HeroService, private messageService: MessageService) {}
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add('HeroesComponent: Seleced hero id=' + hero.id);
-  }
-
+  
   ngOnInit(): void {
     this.getHeroes();
   }
