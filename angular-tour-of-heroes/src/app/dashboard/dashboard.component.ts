4f0d9acfb457f24810477404257eb750d34bbfe7
Zach Estus <zachary.estus@student.awesomeinc.org>
2023-01-25 20:18:43 +0000

    Finished tutorial

--- angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
+++ angular-tour-of-heroes/src/app/dashboard/dashboard.component.ts
@@ -5,11 +5,11 @@ import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: [ './dashboard.component.css' ]
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
-  
+
   constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
@@ -18,6 +18,6 @@ export class DashboardComponent implements OnInit{
 
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
 }
